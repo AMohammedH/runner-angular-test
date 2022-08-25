f1692f8e6501217af10e1b34b46a725d7626e7e9
AMohammedH <ahtesham.dev@gmail.com>
2022-08-25 12:48:59 -0500

    matAutocomplete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, debounceTime, distinctUntilChanged, startWith, filter } from 'rxjs/operators';
+import { HttpClient } from "@angular/common/http";
+
+
 
 @Component({
   selector: 'app-root',
@@ -16,4 +22,51 @@ export class AppComponent {
     'The selected titles should be removable.',
     'This mimics a form element in our application where users assign title metadata to assets, so if you would like to build something that replicates a form submission, feel free to come up with your own solution to how it "saves" the data.'
   ];
+  profileData: any = [];
+  myControl = new FormControl('');
+  filteredOptions: any;
+  filteredData: string[] = [];
+  getFullName: any;
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit() {
+    // Simulation of Api call 
+    // Taking from JSON data (Placing Json files in assets folder is a good practice)
+    this.httpClient.get("assets/data.json").subscribe(data => {
+      this.profileData = data;
+      // Here this logic filters name from response 
+      this.filteredData = this.profileData.map(function (item: any) {
+        return item.name;
+      })
+      // Checking Dropdown data existance 
+      if (this.filteredData.length) {
+        //Listening input field and trigger the filtration
+        this.filteredOptions = this.myControl.valueChanges.pipe(
+          // this is to trigger if entered 3 or more characters
+          filter(res => {
+            return res !== null && res.length >= 3
+          }),
+          startWith(''),
+          debounceTime(100),
+          distinctUntilChanged(),
+          map(value => this._filter(value || '')),
+        );
+      }
+    })
+  }
+
+  // returing the entered Item
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.filteredData.filter((option: any) => option.toLowerCase().includes(filterValue));
+  }
+  onSelect(selectedData: string) {
+    const filterName = this.profileData.filter(function (item: any) {
+      if (item.name == selectedData) {
+        return item;
+      }
+    })
+    this.getFullName = filterName[0].full_name;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
